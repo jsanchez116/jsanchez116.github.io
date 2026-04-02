ed06fdd33cc5fb6d442dc0508cb34c77621fa31f
Jose Sanchez <jjsanchez2003@yahoo.com>
2026-04-02 10:43:06 -0700

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -112,7 +112,7 @@ const CONFIG = {
       link: 'https://example.com',
     },
   ],
-  education: [
+  educations: [
     {
       institution: 'California State University, Stanislaus',
       degree: 'B.S. Biological Sciences, General',
