5ab5ca5992c36f8aea5f21ea28d8b96c59c9e7cb
Jose Sanchez <jjsanchez2003@yahoo.com>
2026-06-10 18:07:50 -0700

    Update sanitized-config.tsx
    Revert to original

--- src/interfaces/sanitized-config.tsx
+++ src/interfaces/sanitized-config.tsx
@@ -142,7 +142,6 @@ export interface SanitizedConfig {
   social: SanitizedSocial;
   resume: SanitizedResume;
   skills: Array<string>;
-  courses: Array<string>;
   experiences: Array<SanitizedExperience>;
   educations: Array<SanitizedEducation>;
   certifications: Array<SanitizedCertification>;
